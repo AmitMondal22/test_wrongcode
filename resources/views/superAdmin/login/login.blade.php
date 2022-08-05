752cc93837542984d7d4471ac3b62ec554a6a044
AmitMondal22 <amit.ndk3@gmail.com>
2022-08-05 20:43:05 +0530

    hello

--- resources/views/superAdmin/login/login.blade.php
+++ resources/views/superAdmin/login/login.blade.php
@@ -45,7 +45,28 @@
 
 <body>
     <!-- Content -->
+    @php
+        function get_client_ip() {
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
+    }
 
+    echo 'User Real IP - '.get_client_ip();
+    @endphp
 
     <div class="container-xxl">
         <div class="authentication-wrapper authentication-basic container-p-y">
